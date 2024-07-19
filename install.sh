aaf162d1aea535e07ab791758488999da0e98ab0
sang <sang.chen@outlook.com>
2024-07-19 16:36:24 +0800

    feat: add chmod for bin

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag -o /usr/local/bin/lxc-iptag
 curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag.service -o /lib/systemd/system/lxc-iptag.service
+chmod +x /usr/local/bin/lxc-iptag
 
 sudo systemctl daemon-reload
 sudo systemctl enable lxc-iptag.service
